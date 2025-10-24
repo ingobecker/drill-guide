6b7828cd3940dfdede3ab5be36c9397a95d441eb
Ingo Becker <ingo@orgizm.net>
2025-10-24 12:34:26 +0200

    Fix typos in script

--- drill-guide.py
+++ drill-guide.py
@@ -7,7 +7,7 @@ handle_edge_to_bolt = {'long': 94.5, 'short': 17}
 top_bolt_to_hole = 38.747
 
 ap = argparse.ArgumentParser(description='Helps to align the tool to drill a hole at a specified distance from the edge.')
-ap.add_argument('-m', '--material', type=float, required=True, help='thinkness of the material used in mm')
+ap.add_argument('-m', '--material', type=float, required=True, help='thickness of the material used in mm')
 ap.add_argument('-d', '--distance', type=float, required=True, help='distance of the hole from the edge in mm')
 ap.add_argument('-a', '--align', type=str, default='long', choices=['long', 'short'], help='handle to use for alignment')
 
@@ -27,5 +27,5 @@ handle_alignment_dist = handle_edge_to_bolt[handle]
 mark_distance = distance + abs(handle_alignment_dist - tri_a)
 mark_distance = round(mark_distance, 1)
 
-print("Material thickness: {}, Hole distance from edge: {}".format(material_thickness, distance))
+print("Material thickness: {} mm, Hole distance from edge: {} mm".format(material_thickness, distance))
 print("Alignment mark ({} handle): {} mm".format(handle, mark_distance))
